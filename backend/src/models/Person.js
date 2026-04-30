500c69f89bb69c9e1248c3da0101fad843e95143
Yafit Lazar <Yafit10@example.com>
2026-04-30 23:47:41 +0300

    final version

--- backend/src/models/Person.js
+++ backend/src/models/Person.js
@@ -1,7 +1,25 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 const Class = require("./Class");
+const isValidPersonId = (id) => {
+  if (!/^\d{9}$/.test(id)) {
+    return false;
+  }
 
+  let sum = 0;
+
+  for (let i = 0; i < 9; i++) {
+    let num = Number(id[i]) * ((i % 2) + 1);
+
+    if (num > 9) {
+      num -= 9;
+    }
+
+    sum += num;
+  }
+
+  return sum % 10 === 0;
+};
 const Person = sequelize.define("Person", {
   firstName: {
     type: DataTypes.STRING,
@@ -26,12 +44,19 @@ const Person = sequelize.define("Person", {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+    validate: {
+      isValidId(value) {
+        if (!isValidPersonId(value)) {
+          throw new Error("Invalid person ID");
+        }
+      }
+    }
   },
   classId: {
     type: DataTypes.INTEGER,
     allowNull: false,
 
-    
+
   }
 });
 
